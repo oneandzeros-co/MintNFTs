ab77db9018a92adf68a03627a900c64a9b262670
Ayman <65835687+ceobitch@users.noreply.github.com>
2022-05-29 01:52:50 -0500

    Fixed go-ipfs

--- start-local-environment.sh
+++ start-local-environment.sh
@@ -9,11 +9,11 @@ fi
 
 # if there's no local ipfs repo, initialize one
 if [ ! -d "$HOME/.ipfs" ]; then
-  npx go-ipfs init
+  npx ipfs init
 fi
 
 echo "Running IPFS and development blockchain"
 run_eth_cmd="npx hardhat node"
-run_ipfs_cmd="npx go-ipfs daemon"
+run_ipfs_cmd="npx ipfs daemon"
 
 npx concurrently -n eth,ipfs -c yellow,blue "$run_eth_cmd" "$run_ipfs_cmd"
